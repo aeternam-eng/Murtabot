3201ac8ce31df860d153f276f85ce62d69cc9d24
Hugo Brandão <hugo.h.b@hotmail.com>
2021-02-23 21:10:01 -0300

    correct mark img url

--- commands/mark.js
+++ commands/mark.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'mark',
     description: 'Esférico',
     execute(message, args) {
-        message.channel.send('Hora da Missa, Esféricos', { files: ['https://imgur.com/a/g6EvDPl.jpg'] });
+        message.channel.send('Hora da Missa, Esféricos', { files: ['https://i.imgur.com/GHoq95X.png'] });
     },
 };
