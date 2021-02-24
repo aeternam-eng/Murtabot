90b71cb389bc35050ad0ef1763bfa8a6dab194f2
Hugo Brandão <hugo.h.b@hotmail.com>
2021-02-23 21:07:44 -0300

    change mark img format

--- commands/mark.js
+++ commands/mark.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'mark',
     description: 'Esférico',
     execute(message, args) {
-        message.channel.send('Hora da Missa, Esféricos', { files: ['https://imgur.com/a/g6EvDPl'] });
+        message.channel.send('Hora da Missa, Esféricos', { files: ['https://imgur.com/a/g6EvDPl.jpg'] });
     },
 };
